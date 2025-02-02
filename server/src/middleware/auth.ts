ef71428e3d3058a4672fd485b07a54cc02e3f509
eddie wang <wang8119@gmail.com>
2025-02-02 18:07:55 +0800

    isAuth middleware

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,40 @@
+import { RequestHandler } from 'express';
+import jwt, { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+import { sendErrorRes } from 'src/utils/helper';
+import UserModel from 'src/models/user';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const isAuth: RequestHandler = async (req, res, next) => {
+  try {
+    const authToken = req.headers.authorization;
+    if (!authToken) return sendErrorRes(res, 'unauthorized request!', 403);
+
+    const token = authToken.split('Bearer')[1];
+
+    req.headers.authorization = token;
+
+    const payload = jwt.verify(token, JWT_SECRET) as { id: string };
+
+    const user = await UserModel.findById(payload.id);
+
+    if (!user) return sendErrorRes(res, 'unauthorized request!', 403);
+
+    req.user = {
+      id: user._id,
+      email: user.email,
+      name: user.name,
+      verified: user.verified,
+    };
+
+    next();
+  } catch (error) {
+    if (error instanceof TokenExpiredError) {
+      return sendErrorRes(res, 'session expired!', 401);
+    }
+    if (error instanceof JsonWebTokenError) {
+      return sendErrorRes(res, 'unauthorized access!', 401);
+    }
+    return sendErrorRes(res, 'unauthorized access!', 403);
+  }
+};
