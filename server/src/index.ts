366a17233b0ce36d191d547bb2ea443ebf51e365
eddie wang <wang8119@gmail.com>
2025-02-06 19:51:13 +0800

    File Upload

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import authRouter from 'routes/auth';
 import 'src/db';
 import 'express-async-errors';
+import 'dotenv/config';
+import path from 'path';
+import formidable from 'formidable';
 
 const app = express();
 
@@ -9,6 +12,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/auth', authRouter);
+app.post('/upload-file', async (req, res) => {
+  const form = formidable({
+    uploadDir: path.join(__dirname, 'public'),
+    filename(name, ext, part, form) {
+      return Date.now() + '_' + part.originalFilename;
+    },
+  });
+
+  await form.parse(req);
+
+  res.send('ok');
+});
 
 app.use(function (err, req, res, next) {
   res.status(500).json({ message: err.message });
